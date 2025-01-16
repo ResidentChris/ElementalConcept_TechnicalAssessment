376ca345513debbf90fe19124733687e23a21924
Chris Walker <chris.walker@arria.com>
2025-01-16 09:56:19 +0000

    Added H2 dependency to allow default tests to pass.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,15 @@ repositories {
 	mavenCentral()
 }
 
+val h2Version = "2.3.232"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.h2database:h2:$h2Version")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
